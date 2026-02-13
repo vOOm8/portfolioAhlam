f39c70edfedc647650e7ee61535fdd9526a847b7
vOOm8 <ahlam.u369@gmail.com>
2026-02-13 18:45:38 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -140,3 +140,17 @@ window.addEventListener('resize', () => {
     resizeCanvas();
     initRain();
 });
+
+// Hamburger Menu Logic
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-links li a").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}));
